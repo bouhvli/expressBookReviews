73f4060f9c741c944730518f5fd75a7d42e2b72b
bouhvli <bouhali.hamza.2@gmail.com>
2024-07-23 17:21:12 +0100

    adding delete

--- final_project/index.js
+++ final_project/index.js
@@ -11,22 +11,22 @@ app.use(express.json());
 app.use(session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req, res, next) {
-  if (req.session.authorization) {
-    const token = req.session.authorization['accessToken']; // Retrieve the token from session
-
-    jwt.verify(token, 'your_jwt_secret', (err, user) => { // Verify the token
-      if (!err) {
-        req.user = user; // Attach user information to the request object
-        next(); // Proceed to the next middleware or route handler
-      } else {
-        return res.status(403).json({ message: "User not authenticated" });
-      }
-    });
+  if(req.session.authorization) {
+    let token = req.session.authorization['accessToken']; // Access Token
+    jwt.verify(token, "access",(err,user)=>{
+        if(!err){
+            req.user = user;
+            next();
+        }
+        else{
+            return res.status(403).json({message: "User not authenticated"})
+        }
+      });
   } else {
-    return res.status(403).json({ message: "User not logged in" });
+      return res.status(403).json({message: "User not logged in"})
   }
 });
- 
+
 const PORT =5000;
 
 app.use("/customer", customer_routes);
